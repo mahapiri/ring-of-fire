ba3aa771cb0e504c3273cf614db2d34e219307c6
Pirathib Mahalingam <112911160+mahapiri@users.noreply.github.com>
2024-05-25 19:12:23 +0200

    edited

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-start-screen',
@@ -8,5 +9,14 @@ import { Component } from '@angular/core';
   styleUrl: './start-screen.component.scss'
 })
 export class StartScreenComponent {
+
+  constructor(private router: Router) {
+
+  }
+  
+  newGame() {
+    //start Game
+    this.router.navigateByUrl('/game');
+  }
 
 }
