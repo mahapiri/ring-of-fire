66d67e4d2b970331d82dd68bc8b4d2f021182aa2
Pirathib Mahalingam <112911160+mahapiri@users.noreply.github.com>
2024-05-25 19:05:44 +0200

    edited startscreen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StartScreenComponent } from './start-screen/start-screen.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StartScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
