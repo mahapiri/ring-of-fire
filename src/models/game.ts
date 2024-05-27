cfcf327b3593f04c5c334b598fe2c2c0300da077
Pirathib Mahalingam <112911160+mahapiri@users.noreply.github.com>
2024-05-27 14:24:43 +0200

    edited

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = [];
+    public players: string[] = ['Hans', 'Max', 'Junus', 'Hans', 'Max'];
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currenPlayer: number = 0;
