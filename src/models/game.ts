483f6281921ba139c1365cc196a884f5673112e7
Pirathib Mahalingam <112911160+mahapiri@users.noreply.github.com>
2024-05-28 12:22:31 +0200

    first playabale version

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = ['Piri', 'Max', 'Piri', 'Max', 'Piri', 'Max', 'Piri', 'Max', 'Piri', 'Max', 'Piri', 'Max'];
+    public players: string[] = [];
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currenPlayer: number = 0;
